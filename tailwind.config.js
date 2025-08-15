0a1cf62aa5a3ed2f049f05b8b690acf8a87e3a1a
Your Name <a0908647929@gmail.com>
2025-08-15 16:40:20 +0800

    First commit.

---
+++ tailwind.config.js
@@ -0,0 +1,40 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+
+        //=====basic color=====
+        Cus_Red: "#811A1F",
+        Cus_Brown: "#400306",
+        Cus_White: "#FFFFFF",
+        Cus_Deep_Brown: "#231815",
+
+        Cus_gold_light: "#D0AF79",
+        Cus_gold: "#E1A23B",
+        Cus_gold_dark: "#B28946",
+      },
+      backgroundImage: {
+        //=====gradient color (for text)=====
+        gold_gradient: "linear-gradient(to bottom, #E1A23B, #B28946)",
+      },
+      fontFamily: {
+        Serif: ['"Noto Serif TC"', "serif"],
+      },
+      screens: {
+        sm: "420px",
+        md: "740px",
+        m_md: "1024px",
+        lg: "1250px",
+      },
+    },
+  },
+  plugins: [],
+};
