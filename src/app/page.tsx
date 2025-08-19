eec35e62ef78e2c5316ce1f646446b8671bf936f
Your Name <a0908647929@gmail.com>
2025-08-19 20:03:55 +0800

    Page debug.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import HomeLoading from "./loading";
 import HomeSmallTitle from "./home/home_small_title_Wu_Sheng";
 import HomeBanner from "./home/home_banner";
 
@@ -6,23 +5,20 @@ export default function Home() {
   return (
     <div>
       <div>
-        {/* <div>
-          <HomeLoading />
-          <div className="fixed inset-0 flex items-center justify-center z-[60] pointer-events-none">
-            <HomeSmallTitle />
-          </div>
-        </div> */}
+        <div className="fixed inset-0 flex items-center justify-center z-[60] pointer-events-none">
+          <HomeSmallTitle />
+        </div>
         <HomeBanner />
-        {/* <HomeContent /> */}
+        <HomeContent />
       </div>
     </div>
   );
 }
 
-// export function HomeContent() {
-//   return (
-//     <div>
-//       <p>HomeContent</p>
-//     </div>
-//   );
-// }
+export function HomeContent() {
+  return (
+    <div>
+      <p>HomeContent</p>
+    </div>
+  );
+}
